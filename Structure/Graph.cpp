8bcc266b5fedb0d1ff819b1a7729d79ee076ae3a
aguitarinajar <89738337+HienMyTruong@users.noreply.github.com>
2022-06-07 18:27:46 +0200

    new comments

--- Structure/Graph.cpp
+++ Structure/Graph.cpp
@@ -12,14 +12,14 @@ public:
 class Graph
 {
 public:
-    // V-> Number of vertices, E-> Number of edges
+    // V represents number of vertices, E, number of edges
     int V, E;
 
-    // graph is represented as an array of edges
+    // array of edges
     Edge *edge;
 };
 
-// Creates a graph with V vertices and E edges
+// creates a graph with V vertices and E edges
 Graph *createGraph(int V, int E)
 {
     Graph *graph = new Graph();
@@ -33,13 +33,37 @@ Graph *createGraph(int V, int E)
 
 // for the unionfind algoritm can be used to check if an undirected graph has a cicle, assuming that it doesn't contain any self loops
 
-int find(int parent[], int index)
+// if we have
+/*
+0
+| \
+|  \
+1---2
+
+and would like to assign parents to each node
+we assign every individual node to -1 in out parent array
+0   1   2
+-1  -1  -1
+
+if we assign 1 as a parent to 0, we change the array value (-1) to the value of the the node (1), such as
+0  1  2
+1 -1 -1
+
+we do the same when directing 1 to 2
+0  1  2
+1  2 -1
+
+we only have 2 left, not assigned to any parent. 2 is the parent in this case.
+Any index assigned with the value -1 is a parent.
+
+*/
+int find(int parent[], int val)
 {
-    if (parent[index] == -1)
+    if (parent[val] == -1)
     {
-        return index;
+        return val;
     }
-    return find(parent, parent[index]);
+    return find(parent, parent[val]);
 }
 
 void Union(int parent[], int subset1, int subset2)
@@ -62,7 +86,6 @@ bool isCycle(Graph *graph)
 
         if (start == finish)
         {
-
             return true;
         }
         Union(parent, start, finish);
