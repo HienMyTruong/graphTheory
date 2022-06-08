af7fbb00cc6c317d264a68fd7e1c4d0455909fef
aguitarinajar <89738337+HienMyTruong@users.noreply.github.com>
2022-06-08 12:03:41 +0200

    bellman ford algoritm

--- Structure/Graph.cpp
+++ Structure/Graph.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Edge
 {
 public:
-    int source, destination;
+    int source, destination, weight;
 };
 
 // a structure to represent a graph
@@ -93,15 +93,65 @@ bool isCycle(Graph *graph)
     return false;
 }
 
-int main()
+// a negative cycle is one in which the overall sum of the cycle becomes negative-
+// introducing weight in this problem since it has certain applications, especially negative weight in this case
+bool isNegativeBellmanFord(Graph *graph, int source)
 {
 
-    /*
-    0
-    | \
-    |  \
-    1---2
+    /* AGORITM: to find if there is a negative weight cycle reachable from the given source
+        1. distances from the source is initialized to all vertices as infinite (maxint)
+        and distance to the source itself as 0. An array of distances of size abs(V) with
+        all values as maxint (infinite) except the value with the sourcevertex-index (distances[source]).
+
+        2. Calculate the shortest distances by doing
+
+
     */
+    int V = graph->V;
+    int E = graph->E;
+    int distances[abs(V)];
+    // initializing distances from source to all other nodes as infinite
+    for (int i = 0; i < V; i++)
+    {
+        distances[i] = INT_MAX;
+    }
+    distances[source] = 0;
+
+    for (int i = 1; i <= abs(V) - 1; i++)
+    {
+        for (int j = 0; j < E; j++)
+        {
+            int js = graph->edge[j].source;
+            int jd = graph->edge[j].destination;
+            int weight = graph->edge[j].weight;
+            if (distances[js] != INT_MAX && distances[js] + weight < distances[jd])
+            {
+                distances[jd] = distances[js] + weight;
+            }
+        }
+    }
+
+    for (int i = 0; i < E; i++)
+    {
+        int js = graph->edge[i].source;
+        int jd = graph->edge[i].destination;
+        int weight = graph->edge[i].weight;
+        if (distances[js] != INT_MAX && distances[js] + weight < distances[jd])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void testIsCycle()
+{
+    /*
+   0
+   | \
+   |  \
+   1---2
+   */
 
     int V = 3;
     int E = 3;
@@ -123,6 +173,64 @@ int main()
         cout << "contains cycle";
     else
         cout << "doesn't contain cycle";
+}
 
+void testNegativeCycleBellamnFord()
+{
+
+    int V = 5;
+    int E = 8;
+    Graph *graph = createGraph(V, E);
+
+    // adding edge 0-1
+    graph->edge[0].source = 0;
+    graph->edge[0].destination = 1;
+    graph->edge[0].weight = -1;
+
+    // add edge 0-2
+    graph->edge[1].source = 0;
+    graph->edge[1].destination = 2;
+    graph->edge[1].weight = 4;
+
+    // add edge 1-2
+    graph->edge[2].source = 1;
+    graph->edge[2].destination = 2;
+    graph->edge[2].weight = 3;
+
+    // add edge 1-3
+    graph->edge[3].source = 1;
+    graph->edge[3].destination = 3;
+    graph->edge[3].weight = 2;
+
+    // add edge 1-4
+    graph->edge[4].source = 1;
+    graph->edge[4].destination = 4;
+    graph->edge[4].weight = 2;
+
+    // add edge 3-2
+    graph->edge[5].source = 3;
+    graph->edge[5].destination = 2;
+    graph->edge[5].weight = 5;
+
+    // add egde 3-1
+    graph->edge[6].source = 3;
+    graph->edge[6].destination = 1;
+    graph->edge[6].weight = 1;
+
+    // add edge 4-3
+    graph->edge[7].source = 4;
+    graph->edge[7].destination = 3;
+    graph->edge[7].weight = -3;
+
+    if (isNegativeBellmanFord(graph, 0))
+        cout << "Yes";
+    else
+        cout << "No";
+}
+
+int main()
+{
+    // testIsCycle();
+    testNegativeCycleBellamnFord();
     return 0;
 }
